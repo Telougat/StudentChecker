fc697016847d51348606ffd5b88bcb5d07957c59
Marvin COUSIN <marvin.cousin05@gmail.com>
2020-07-30 17:06:43 +0200

    modif getter setter utilisateur

--- src/main/java/org/model/Utilisateur.java
+++ src/main/java/org/model/Utilisateur.java
@@ -41,56 +41,24 @@ public class Utilisateur extends DB {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getNom() {
         return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
     }
 
     public String getPrenom() {
         return prenom;
     }
 
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
     public String getMail() {
         return mail;
     }
 
-    public void setMail(String mail) {
-        this.mail = mail;
+    public Groupe getGroupe() {
+        return groupe;
     }
 
-    public String getMdp() {
-        return mdp;
-    }
-
-    public void setMdp(String mdp) {
-        this.mdp = mdp;
-    }
-
-    public int getId_groupe() {
-        return id_groupe;
-    }
-
-    public void setId_groupe(int id_groupe) {
-        this.id_groupe = id_groupe;
-    }
-
-    public int getId_ecole() {
-        return id_ecole;
-    }
-
-    public void setId_ecole(int id_ecole) {
-        this.id_ecole = id_ecole;
+    public Ecole getEcole() {
+        return ecole;
     }
 
     // METHODES
