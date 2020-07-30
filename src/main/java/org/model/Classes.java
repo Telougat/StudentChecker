955ea088e6b40132ae01ba7caeaf1546831d3c35
Marvin COUSIN <marvin.cousin05@gmail.com>
2020-07-30 16:17:36 +0200

    modif groupe + classes

--- src/main/java/org/model/Classes.java
+++ src/main/java/org/model/Classes.java
@@ -3,8 +3,41 @@ package org.model;
 import java.util.List;
 
 public class Classes extends DB {
+
+    // ATTRIBUTS
     private int id;
     private String nom;
     private int ecole;
 
+    // CONSTRUCTEUR
+    public Classes(int id, String nom, int ecole) {
+        this.id = id;
+        this.nom = nom;
+        this.ecole = ecole;
+    }
+
+    // GETTER ET SETTER
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getEcole() {
+        return ecole;
+    }
+
+    public void setEcole(int ecole) {
+        this.ecole = ecole;
+    }
 }
--- src/main/java/org/model/Groupe.java
+++ src/main/java/org/model/Groupe.java
@@ -1,6 +1,31 @@
 package org.model;
 
 public class Groupe {
+
+    // ATTRIBUTS
     private int id;
     private String label;
+
+    // CONSTRUCTEUR
+    public Groupe(int id, String label) {
+        this.id = id;
+        this.label = label;
+    }
+
+    // GETTER ET SETTER
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
 }
