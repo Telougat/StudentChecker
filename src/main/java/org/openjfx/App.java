dab5978379f9204a1bab067dc548062ae0c04b6d
Marvin COUSIN <marvin.cousin05@gmail.com>
2020-07-31 14:18:06 +0200

    modif listclasses

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -221,30 +221,20 @@ public class App extends Application {
     public VBox getPageGestionClasse()
     {
         VBox layoutPageGestionClasse = new VBox();
-
         Label labelTitre = new Label("Liste des classes");
         labelTitre.setFont(new Font("Arial", 24));
+        ListView listViewClasse = new ListView();
 
-        String classe1 = new String("classe1");
-        String classe2 = new String("classe2");
-        String classe3 = new String("classe3");
+        for (Classe classe : CESI.classe) {
+            //classe.getNom();
+            listViewClasse.getItems().add(classe.getNom());
+        }
+
         Button addClasse = new Button("Ajouter une Classe");
         Button modifClasse = new Button("Modifier une Classe");
         Button deleteClasse = new Button("Supprimer une Classe");
 
-        ObservableList<String> classes = FXCollections.observableArrayList(classe1, classe2, classe3);
-
-        ListView<String> ClasseView = new ListView<String>(classes);
-
-        //ClasseView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-
-        //ClasseView.getSelectionModel().selectIndices(1, 2);
-
-        //ClasseView.getFocusModel().focus(1);
-
-
-
-        layoutPageGestionClasse.getChildren().addAll(labelTitre, ClasseView, addClasse, modifClasse, deleteClasse);
+        layoutPageGestionClasse.getChildren().addAll(labelTitre, listViewClasse, addClasse, modifClasse, deleteClasse);
 
         return layoutPageGestionClasse;
     }
